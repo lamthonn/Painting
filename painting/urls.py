b545e3a5ac4c0c7850133c21e3f276f0d3c1d9bb
dinhhai <dinhthihai130903@gmail.com>
2023-06-04 22:20:47 +0700

    hai xinh dep vai o

--- painting/urls.py
+++ painting/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('explore/', views.painting_list,name='list'),
     path('detail/<int:pk>/',views.painting_detail,name='detail'),
     path('search/',views.painting_search,name='search'),
-    path('blog/',views.blog,name='blog')
+    path('blog/',views.blog,name='blog'),
+    path('news/',views.news,name='news'),
+    path('contact/',views.contact,name='contact'),
+    path('admin_list/',views.admin_list,name='admin_list'),
+    path('edit_pictures/<int:pk>/',views.edit_pictures,name='edit_pictures'),
+    path('update_pictures/<int:pk>/',views.update_pictures,name='update_pictures'),
+    path('delete_pictures/<int:pk>/',views.delete_pictures,name='delete_pictures')
 ]
