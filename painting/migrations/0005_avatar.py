b2544e3e4fd50228e82647ac758876b972f96314
lamthonn <lamvu180903@gmail.com>
2023-06-13 16:54:12 +0700

    lam vu

--- painting/migrations/0005_avatar.py
+++ painting/migrations/0005_avatar.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('painting', '0004_like_comment'),
+        # ('painting', '0004_like_comment'),
     ]
 
     operations = [
